5e7f49eb2dd68a178552c704b51e232c00845517
logtwin <109973598+logtwin@users.noreply.github.com>
2022-07-25 12:07:25 -0300

    addd index.php

---
+++ index.php
@@ -0,0 +1,115 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <title>Projetos</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <style>.disclaimer {display: none !important;}</style>
+    <!-- Bootstrap CSS v5.0.2 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  </head>
+  <body>
+    <div id="home">
+      <div class="mx-auto w-auto d-flex flex-column" style="margin-top: 100px;">
+          <h1 class="text-center mb-0 w-auto" id="projeto">PROJETOS</h1>
+          <h5  class="text-center" id="nome_home">Tiago Oliveira</h5>
+
+          <div id="seta" class="mx-auto" style="margin-top: 185px;">
+            <div>
+              <img src="img/seta.png" alt="seta" id="seta">
+            </div>
+          </div>
+      </div>
+    </div>
+      
+
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <div class="mt-3 cards">
+              <div class="card col-sm-2 col-md-3" style="width: 18rem;">
+                <!--<img src="..." class="card-img-top" alt="...">-->
+                <div class="card-body">
+                  <h5 class="card-title">Calculadora</h5>
+                  <p class="card-text">Calculadora simples feita com javascript</p>
+                  <a href="calculadora/index.html" class="btn btn-amarelo">Testar</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+          <div class="col">
+             <div class="mt-3 cards">
+              <div class="card col-sm-2 col-md-3" style="width: 18rem;">
+                <!--<img src="..." class="card-img-top" alt="...">-->
+                <div class="card-body">
+                  <h5 class="card-title">Calculadora imc</h5>
+                  <p class="card-text">Calculadora que calcular imc feita com javascript</p>
+                  <a href="calculadora-imc/index.html" class="btn btn-amarelo">Testar</a>
+                </div>
+              </div>
+            </div>
+          </div>         
+
+          <div class="col">
+            <div class="mt-3 cards">
+              <div class="card col-sm-2 col-md-3" style="width: 18rem;">
+                <!--<img src="..." class="card-img-top" alt="...">-->
+                <div class="card-body">
+                  <h5 class="card-title">Jogo mata-mosquito</h5>
+                  <p class="card-text">Jogo mata-mosquito. Tente matar o máximo de mosquito que conseguir</p>
+                  <a href="jogo-mata-mosquito/index.html" class="btn btn-amarelo">Testar</a>
+                </div>
+              </div>
+            </div>
+          </div>
+         
+          <div class="col">
+            <div class="mt-3 cards">
+              <div class="card col-sm-2 col-md-3" style="width: 18rem;">
+                <!--<img src="..." class="card-img-top" alt="...">-->
+                <div class="card-body">
+                  <h5 class="card-title">Lista de tarefas</h5>
+                  <p class="card-text">Web-app que lista tarefas a fazer</p>
+                  <a href="lista_de_tarefas/index.html" class="btn btn-amarelo">Testar</a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col">
+            <div class="mt-3 cards">
+              <div class="card col-sm-2 col-md-3" style="width: 18rem;">
+                <!--<img src="..." class="card-img-top" alt="...">-->
+                <div class="card-body">
+                  <h5 class="card-title">Send email</h5>
+                  <p class="card-text">Aplicação para enviar emails feito com php</p>
+                  <a href="app_email/index.php" class="btn btn-amarelo">Testar</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+          <div class="col">
+            <div class="mt-3 cards">
+              <div class="card col-sm-2 col-md-3" style="width: 18rem;">
+                <!--<img src="..." class="card-img-top" alt="...">-->
+                <div class="card-body">
+                  <h5 class="card-title">Listar endereço pelo cep</h5>
+                  <p class="card-text">Aplicação feita com php e consumindo api viacep recebendo e tratando dados json e mostrando para o usuário</p>
+                  <a href="pesquisa_endereco/index.html" class="btn btn-amarelo">Testar</a>
+                </div>
+              </div>
+            </div>
+          </div>
+         
+
+          <footer class="mt-3">
+            <p class="text-center" style="color: rgba(197, 197, 197, 0.856);">Tiagol09 - Todos os direitos reservados</p>
+          </footer>
+      </div>
+
+  </body>
+</html>
